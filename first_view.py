e0723736c95f9274bbd722e454a3c738d743217e
victor-sc12 <victor.suquilanda@udla.edu.ec>
2025-02-03 22:36:13 -0500

    Se corrigió el bug de los Entry Widget mal ubicados (solo era una extensión innecesaria por el columnspace del label xd)

--- first_view.py
+++ first_view.py
@@ -6,20 +6,35 @@ from PIL import Image, ImageTk
 class FirstView(ttk.Frame):
     def __init__(self, cointainer, music):
         super().__init__(cointainer)
-        self.music = music
+        self.music = music # Inicializar propiedad self.music con el objeto (music) enviado como paramétro
+
+        # Inicializar atributos (img, artista, songname) del objeto dentro de esta clase:
         self.music_image = ImageTk.PhotoImage(Image.open(self.music.get_image()).resize((320,320)))
         self.artist_name = self.music.get_artist()
         self.song_name = self.music.get_name()
-        self.char_values = []
-        self.char_values_entry = []
-        self.input_frame = ttk.Frame()
+
+        self.char_values = [] # lista para almacenar los char de songname
+        self.char_values_entry = [] # lista para los valores de entrada
+
+        # Definición del frame donde se ubicaran los 'Entry Widgets' para los datos de entrada:
+        self.input_frame = ttk.Frame(self)
         self.input_frame.grid()
-        self.boton = ttk.Button(self.input_frame, text="Comprobar", command=self.process_entry)
-        self.boton.grid(row=2, columnspan=3)
+
+        # Definición del label donde se ubicarán los mensajes de alerta
+        self.msg_label = ttk.Label(self, text='')
+        self.msg_label.grid(row=1)
+
+        # Definición del Button widget para procesar el dato ingresado
+        self.boton = ttk.Button(self, text="Comprobar", command=self.process_entry)
+        self.boton.grid(row=2)
+
+        # Colocación de los atributos img, artistname dentro del frame principal
         self.label_img = ttk.Label(self, image=self.music_image)
         self.label_img.grid()
         self.label_title = ttk.Label(self, text=self.artist_name)
         self.label_title.grid()
+
+        # Declaración de la función que coloca los Entry Widgets según el nombre de la canción
         self.put_spaces()
 
     def process_entry(self):
@@ -29,9 +44,11 @@ class FirstView(ttk.Frame):
             i+=1
         # self.char_values_entry = [v for v in self.char_values_entry if v != '']
         if self.char_values_entry == self.char_values:
-            ttk.Label(self.input_frame, text="Well Done").grid(row = 1)
+            self.msg_label.configure(text='Well Done')
+            #ttk.Label(self.input_frame, text="Well Done").grid(row = 1)
         else:
-            ttk.Label(self.input_frame, text="Error. Try again").grid(row=1)
+            self.msg_label.configure(text='Error. Try again')
+            #ttk.Label(self.input_frame, text="Error. Try again").grid(row=1)
         print(self.char_values_entry)
         print(self.char_values)
         self.char_values_entry = []
@@ -46,7 +63,7 @@ class FirstView(ttk.Frame):
                 self.char_values.append(char)
                 self.char_values_entry.append(tk.StringVar())
                 ttk.Entry(self.input_frame, width=5, textvariable=self.char_values_entry[j]).grid(row=0, column=i)
-                print(j, self.char_values_entry[j])
+                #print(j, self.char_values_entry[j])
                 j+=1
             else:
                 ttk.Label(self.input_frame, width=5).grid(row=0, column=i)
